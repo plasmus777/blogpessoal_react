203b5dccadf946109435e55914286f0ef8af106a
Fernando Lopes <118869201+plasmus777@users.noreply.github.com>
2024-02-28 14:49:28 -0300

    Criação de uma foto padrão para usuários

--- src/components/postagens/cardPostagem/CardPostagem.tsx
+++ src/components/postagens/cardPostagem/CardPostagem.tsx
@@ -10,7 +10,9 @@ function CardPostagem({ post }: CardPostagemProps) {
         <div className='border-green-900 border flex flex-col rounded overflow-hidden justify-between'>
             <div className='bg-lime-500 size-full'>
                 <div className="flex w-full bg-green-800 py-2 px-4 items-center gap-4">
-                    <img src={post.usuario?.foto} className='h-12 rounded-full' alt="" />
+                    {post.usuario?.foto != null && post.usuario?.foto != ''
+                    ? <img src={post.usuario?.foto} className='h-12 rounded-full' alt="" />
+                    : <img src='https://pluspng.com/img-png/user-png-icon-download-icons-logos-emojis-users-2240.png' className='h-12 rounded-full' alt="" />}
                     <h3 className='text-lg font-bold text-center uppercase text-white'>{post.titulo}</h3>
                 </div>
                 <div>
